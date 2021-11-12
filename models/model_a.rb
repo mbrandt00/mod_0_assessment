ca1379f0442fd8a915c82d6c59ff87623eb36b85
Michael Brandt <msbrandt00@gmail.com>
2021-11-12 10:01:36 -0700

    Add hash on macbook pro info

---
+++ models/model_a.rb
@@ -0,0 +1,6 @@
+# Part III
+
+macbook_pro = {'OS' => 'macOS Big Sur',
+  'Chip' => 'Apple M1', 'Memory' => 8, 'Storage' => 245.11,
+'shiny' => true, 'new' => true, 'applications' => ['mail', 'slack', 'zoom', 'spotify'],
+'accesories' => ['external monitor', 'kvm switch', 'usb-c hub']}
