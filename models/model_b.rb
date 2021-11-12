ceefc67b70cd624c0572c6bae53f4e89bec4fda2
Michael Brandt <msbrandt00@gmail.com>
2021-11-12 10:11:07 -0700

    Add house hash

---
+++ models/model_b.rb
@@ -0,0 +1,12 @@
+aspen_house = {
+  'sale_price' => 9000000,
+  'square_feet' => 2126,
+  'adress' => '2315 Thompson Road, Aspen CO 81611',
+  'Original Owners' => 'John Doe',
+  'remote' =>  true,
+  'ski-friendly' => true,
+  'years_of_major_remodels' => [1930, 1950, 1974, 1990]
+  'Rooms in House' => ['4 bedrooms', '3 baths', '1 garage]
+  #For fun, the last one would be a lot better as a hash so
+  'Rooms' => {'Bathrooms': 3, 'Bedrooms': 5, 'Garages': 1}
+}
