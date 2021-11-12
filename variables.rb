547d3a792766bf9864e864574fc653548b79aa5f
Michael Brandt <msbrandt00@gmail.com>
2021-11-12 09:53:05 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,19 @@
+# Part II
+
+animal = 'golden_retriever'
+
+is_cuddly = true
+
+animal = 'gorilla'
+
+num_8 = 200
+
+num_3 = 600
+
+quotient = num_3 / num_8
+
+favorite_films = ['Shawshank Redemption', 'Ratatouillie', 'Phantom Thread']
+
+favorite_films.shift # or alternatively favorite_films.delete_at(0)
+
+puts animal.length
