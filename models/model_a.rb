034b8733804e3d785a21363725b64ae2307accfe
Michael Brandt <msbrandt00@gmail.com>
2021-11-15 18:48:00 -0700

    Reform Hash

--- models/model_a.rb
+++ models/model_a.rb
@@ -1,6 +1,11 @@
 # Part III
 
-macbook_pro = {'OS' => 'macOS Big Sur',
-  'Chip' => 'Apple M1', 'Memory' => 8, 'Storage' => 245.11,
-'shiny' => true, 'new' => true, 'applications' => ['mail', 'slack', 'zoom', 'spotify'],
+macbook_pro = {
+  'OS' => 'macOS Big Sur',
+  'Chip' => 'Apple M1',
+   'Memory' => 8,
+   'Storage' => 245.11,
+'shiny' => true,
+'new' => true,
+'applications' => ['mail', 'slack', 'zoom', 'spotify'],
 'accesories' => ['external monitor', 'kvm switch', 'usb-c hub']}
