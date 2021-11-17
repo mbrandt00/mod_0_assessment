5341a900c0858b037d80cfad8fdece565363aab7
Michael Brandt <msbrandt00@gmail.com>
2021-11-17 12:01:24 -0700

    Fix syntax error

--- models/model_b.rb
+++ models/model_b.rb
@@ -5,8 +5,8 @@ aspen_house = {
   'Original Owners' => 'John Doe',
   'remote' =>  true,
   'ski-friendly' => true,
-  'years_of_major_remodels' => [1930, 1950, 1974, 1990]
-  'Rooms in House' => ['4 bedrooms', '3 baths', '1 garage]
+  'years_of_major_remodels' => [1930, 1950, 1974, 1990],
+  'Rooms in House' => ['4 bedrooms', '3 baths', '1 garage'],
   #For fun, the last one would be a lot better as a hash so
   'Rooms' => {'Bathrooms': 3, 'Bedrooms': 5, 'Garages': 1}
 }
