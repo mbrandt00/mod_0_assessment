3d8f76c6b162642bd6daf7b86862a69b1ff290da
Michael Brandt <msbrandt00@gmail.com>
2021-11-17 12:00:53 -0700

    Fix indentation

--- models/model_a.rb
+++ models/model_a.rb
@@ -3,9 +3,12 @@
 macbook_pro = {
   'OS' => 'macOS Big Sur',
   'Chip' => 'Apple M1',
-   'Memory' => 8,
-   'Storage' => 245.11,
-'shiny' => true,
-'new' => true,
-'applications' => ['mail', 'slack', 'zoom', 'spotify'],
-'accesories' => ['external monitor', 'kvm switch', 'usb-c hub']}
+  'Memory' => 8,
+  'Storage' => 245.11,
+  'shiny' => true,
+  'new' => true,
+  'applications' => ['mail', 'slack', 'zoom', 'spotify'],
+  'accesories' => ['external monitor', 'kvm switch', 'usb-c hub']
+}
+
+puts macbook_pro.keys 
